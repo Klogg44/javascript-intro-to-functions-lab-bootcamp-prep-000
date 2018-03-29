1bbf2d7e8c030adc5ed7947463061717dbff191b
Spencer Croy <spencerccroy@gmail.com>
2018-03-29 19:32:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you"
   } else if (string.toUpperCase() === string) {
-    
-    
+    return "YES INDEED"
+  } else if (string === "I lover you, Grandma.") {
+    return "I love you, too."
   }
 }
